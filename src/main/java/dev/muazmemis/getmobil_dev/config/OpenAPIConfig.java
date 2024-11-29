7c5a894face2819d68ac71bb75f57c4f4ec014e2
Muaz MEMİŞ <muazmemis@gmail.com>
2024-11-29 18:43:41 +0300

    bugfix swagger

--- src/main/java/dev/muazmemis/getmobil_dev/config/OpenAPIConfig.java
+++ src/main/java/dev/muazmemis/getmobil_dev/config/OpenAPIConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,17 +15,18 @@ import java.util.List;
 public class OpenAPIConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI customOpenAPI(@Value("${server.servlet.context-path}") String contextPath) {
         return new OpenAPI()
                 .servers(List.of(
-                        new Server().url("http://localhost:8080").description("localhost"),
-                        new Server().url("http://localhost:8081").description("localhost2"),
-                        new Server().url("http://aws_ip_address:8080").description("AWS")))
+                        new Server().url("http://ec2-3-238-15-179.compute-1.amazonaws.com:8080" + contextPath).description("AWS"),
+                        new Server().url("http://localhost:8080" + contextPath).description("localhost"),
+                        new Server().url("http://localhost:8081" + contextPath).description("localhost2")))
                 .info(new Info().title("GetMobil API")
                         .description("This is the REST API for GetMobil")
                         .version("1.0.0")
                         .contact(new Contact().name("GetMobil").url("https://getmobil.com").email("info@getmobil.com"))
                         .license(new License().name("Apache 2.0").url("https://apache.org")));
     }
+
 
 }
